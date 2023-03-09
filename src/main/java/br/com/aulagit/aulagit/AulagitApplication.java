07423091879c7196d0f71e28b076435287ffb659
Parrot Live user <user@localhost.localdomain>
2023-03-09 22:06:22 +0000

    criar projeto

---
+++ src/main/java/br/com/aulagit/aulagit/AulagitApplication.java
@@ -0,0 +1,13 @@
+package br.com.aulagit.aulagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulagitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulagitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aulagit/aulagit/AulagitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aulagit.aulagit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulagitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
